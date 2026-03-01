923a04ebd67f8312cfbfba65535ace2f956fb67a
kai <jiankaiz479@gmail.com>
2026-03-01 16:17:45 +0800

    測試合成版本

--- ai_app/urls.py
+++ ai_app/urls.py
@@ -4,6 +4,6 @@ from .views import RemoveBgView, TryCombineView
 urlpatterns = [
     # API 路徑 (對應 Excel 定義)
     path('virtual_try_on/clothes/remove_bg', RemoveBgView.as_view(), name='remove_bg'),
-    path('api/try_combine', TryCombineView.as_view(), name='try_combine'),
+    path('virtual_try_on/fitting/generate', TryCombineView.as_view(), name='try_combine'),
     
 ]
