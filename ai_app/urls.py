2a1b6958ab547828c5ee6eb4cdfa9a364b0e25e9
kai <jiankaiz479@gmail.com>
2026-04-20 22:49:35 +0800

    修改人物去背內容

--- ai_app/urls.py
+++ ai_app/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import RemoveBgView, TryCombineView
+from .views import RemoveBgView, TryCombineView,ReconstructView
 
 urlpatterns = [
     # API 路徑 (對應 Excel 定義)
     path('virtual_try_on/clothes/remove_bg', RemoveBgView.as_view(), name='remove_bg'),
     path('virtual_try_on/fitting/generate', TryCombineView.as_view(), name='try_combine'),
    # path('virtual_try_on/fitting/tryon_3d_physics', Reconstruct_3D.as_view(), name='try_combine'),
-   # path('virtual_try_on/fitting/modules', SMPLReconstructView.as_view(), name='SMPLReconstructView'),
+    path('virtual_try_on/fitting/modules', ReconstructView.as_view(), name='sReconstructView'),
 ]
